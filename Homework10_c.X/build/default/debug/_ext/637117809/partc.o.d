87444b544b6501f1c105cb6ac7ec86ab3b059f6b
Caio Albuquerque <96552173+caiowastaken@users.noreply.github.com>
2023-05-15 01:04:54 -0700

    Delete Homework10_c.X directory

--- Homework10_c.X/build/default/debug/_ext/637117809/partc.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/_ext/637117809/partc.o:  \
-	C:/Users/caioa/OneDrive/Desktop/partc.ASM  \
-
--- Homework10_c.X/build/default/production/_ext/637117809/partc.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/_ext/637117809/partc.o:  \
-	C:/Users/caioa/OneDrive/Desktop/partc.ASM  \
-
